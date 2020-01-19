a5e6d7642ab5cd854b5c75d15d2cab583b79be59
jchum <jchum@GEORGE-ALIENWARE.hsd1.ca.comcast.net>
2020-01-19 04:08:14 -0800

    Finished displayBoard()

--- Checkers/src/checkers/Board.java
+++ Checkers/src/checkers/Board.java
@@ -4,8 +4,8 @@ public class Board {
 	private Square[][] board;
 	private final int BOARD_SIZE = 8;
 	private final int NUM_OF_PIECES = 12;
-	private final int SCALE_FACTOR = 6;
-	
+	private final int SCALE_FACTOR = 4; //MUST BE AN EVEN INTEGER
+
 	public Board() {
 		board = new Square[BOARD_SIZE][BOARD_SIZE];
 		setBoard();
@@ -28,15 +28,37 @@ public class Board {
 	}
 	
 	public void displayBoard() {
+		int count = 1;
 		for(int r = 0; r <= SCALE_FACTOR * BOARD_SIZE; r++) {
 			for(int c = 0; c <= SCALE_FACTOR * BOARD_SIZE; c++) {
-				if(board[r][c].hasPiece()) {
-					System.out.print(board[r][c].getPiece() + "\t");
+				if(r % SCALE_FACTOR == 0) {
+					if(c % SCALE_FACTOR == 0) {
+						System.out.print("+");
+					} else {
+						System.out.print("-");
+					}
+				} else if(r % SCALE_FACTOR == SCALE_FACTOR / 2 && (r + c) % (2 * SCALE_FACTOR) == 0) {
+					if(board[r / SCALE_FACTOR][c / SCALE_FACTOR].hasPiece()) {
+						System.out.print(board[r / SCALE_FACTOR][c / SCALE_FACTOR].getPiece());
+					} else {
+						System.out.print("/");
+					}
+				} else if(r % SCALE_FACTOR == 1 && (r + c) % (2 * SCALE_FACTOR) == SCALE_FACTOR + 2) {
+					System.out.printf("%2d", count++);
+					c++;
+				} else if(r % (2 * SCALE_FACTOR) >= SCALE_FACTOR + 1 && r % (2 * SCALE_FACTOR) <= (2 * SCALE_FACTOR) - 1 && c % (2 * SCALE_FACTOR) >= 1 && c % (2 * SCALE_FACTOR) <= SCALE_FACTOR - 1) {
+					System.out.print("/");
+				} else if(r % (2 * SCALE_FACTOR) >= 1 && r % (2 * SCALE_FACTOR) <= SCALE_FACTOR - 1 && c % (2 * SCALE_FACTOR) >= SCALE_FACTOR + 1 && c % (2 * SCALE_FACTOR) <= (2 * SCALE_FACTOR) - 1) {
+					System.out.print("/");
+				} else if(c % SCALE_FACTOR == 0) {
+					System.out.print("|");
 				} else {
-					System.out.print("//" + "\t");
+					System.out.print(" ");
 				}
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
+	
 }
